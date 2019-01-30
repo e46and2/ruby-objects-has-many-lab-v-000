563a356e218e2470a935ca17375e4747d50f1eb9
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-01-30 22:33:20 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,9 +19,9 @@ class Author
   end
   
   def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
-    post.author = self
+    new_post = Post.new(post_title)
+    @posts << new_post
+    new_post.author = self
     @@post_count += 1
   end
   
